24be542e0eca36571a709c9bfe3c19cc0a45cb45
alduin10 <ryantomaztalavieira@gmail.com>
2025-06-17 12:53:19 -0300

    17_06_2025
    Comecei :)

---
+++ src/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './Login.css'
+
+function Login() {
+    <input type='text' value={Eight_password} onChange={(e) => setEight_password(e.target.value)}/>
+
+    <button>Revelar Senha</button>
+    <button>Esqueci Minha Senha</button>
+    <button>entrar</button>
+}
+
+export default Login
