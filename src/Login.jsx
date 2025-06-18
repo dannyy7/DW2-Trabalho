143de2713d4aee689c90c45fc7366fb686091323
dannyy7 <163425282+dannyy7@users.noreply.github.com>
2025-06-18 03:41:29 -0300

    18_06_2025

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
-import './Login.css'
+import { useState } from 'react';
+import './Login.css';
 
 function Login() {
-    <input type='text' value={Eight_password} onChange={(e) => setEight_password(e.target.value)}/>
+  const [eightPassword, setEightPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    <button>Revelar Senha</button>
-    <button>Esqueci Minha Senha</button>
-    <button>entrar</button>
+  function mostrarSenha() {
+    setShowPassword(!showPassword);
+  }
+
+  function esqueciSenha() {
+    alert('Recuperação de senha enviada para seu e-mail!'); // mudar depois
+  }
+
+  function entrar() {
+    alert(`Tentando entrar com senha: ${eightPassword}`); // mudar depois
+  }
+
+  let tipoInput;
+  if (showPassword) {
+    tipoInput = 'text';
+  } else {
+    tipoInput = 'password';
+  }
+
+  let textoBotaoMostrarSenha;
+  if (showPassword) {
+    textoBotaoMostrarSenha = 'Ocultar Senha';
+  } else {
+    textoBotaoMostrarSenha = 'Revelar Senha';
+  }
+
+  return (
+    <div className="LoginContainer">
+      <input
+        type={tipoInput}
+        value={eightPassword}
+        onChange={(e) => setEightPassword(e.target.value)}
+        placeholder="Digite sua senha de 8 dígitos"
+      />
+      <br />
+      <button onClick={mostrarSenha}>{textoBotaoMostrarSenha}</button>
+      <button onClick={esqueciSenha}>Esqueci Minha Senha</button>
+      <button onClick={entrar}>Entrar</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import App from './Login.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Login from './Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
-)
+);
