d985321fe8dbe6698c4a05cb4c3c6566534cf877
dannyy7 <163425282+dannyy7@users.noreply.github.com>
2025-06-27 10:00:29 -0300

    27-06-25Manhã

--- src/pages/acesso/login/PaginaPrincipal.jsx
+++ src/pages/acesso/login/PaginaPrincipal.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react"
+
 export default function PaginaPrincipal(){
-    return(<p>uifguigddfiubdf</p>)
+    const[PaginaCriarTarefa, setPaginaCriarTarefa] = useState(false)
+    const[CategoriaTarefa, setCategoriaTarefa] = useState(false)
+    function CriarTarefa(){
+        setPaginaCriarTarefa(true)
+    }
+    function EscolherCategoria(){
+        setCategoriaTarefa(true)
+    }
+    let Categoria = null
+    let NomedaTarefa = null
+    if (PaginaCriarTarefa){
+        NomedaTarefa = 
+        <>
+        <input type="text" id="NomeTarefa"/>
+        <button onClick={EscolherCategoria}>EscolherCategoria</button>
+        </>
+
+
+        if (CategoriaTarefa){
+            Categoria = 
+            <>            
+            <button>Fixo</button>
+            <button>Variavel</button>
+            </>
+
+        }
+    }
+    function EditarTarefa(){
+
+    }
+    function ExcluirTarefa(){
+
+    }
+    function Filtro(){
+
+    }
+    return(
+        <>
+        <input type="text" name="BarraPesquisa" id="Pesquisar" placeholder="pesquisar"/>
+        <button onClick={CriarTarefa}>+</button>
+        <button onClick={Filtro}>Filtrar</button>
+        {NomedaTarefa}
+        {Categoria}
+        </>
+    )
+
 }
+
+//<button onClick={EditarTarefa}>editar</button>
+//<button onClick={ExcluirTarefa}>excluir</button>
