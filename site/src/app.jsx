6289ee5b347f11b58d0de8cc0287b87254b2ce78
alduin10 <ryantomaztalavieira@gmail.com>
2025-06-27 17:57:51 -0300

    despesasVar

--- site/src/app.jsx
+++ site/src/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PaginaPrincipal from './PaginaPrincipal';  // Sua página inicial
-import Login from './Login'; // A página para onde o botão vai te levar
+import PaginaPrincipal from './pages/home/PaginaPrincipal';  // Sua página inicial
+import Login from './pages/acesso/login/Login'; // A página para onde o botão vai te levar
+import DespVar from './pages/despVariaveis/despVar';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/PaginaPrincipal" element={<PaginaPrincipal />} />
+      <Route path="/DespVar" element={<DespVar />} />
     </Routes>
   );
 }
--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './pages/acesso/login/app.jsx'; // Importe o App onde a navegação é configurada
+import App from './app.jsx'; // Importe o App onde a navegação é configurada
 
 const root = createRoot(document.getElementById('root'));
 root.render(
