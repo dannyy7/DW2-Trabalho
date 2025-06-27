6b4f7c821b50ce64d3670f1a320e1434a245c304
dannyy7 <163425282+dannyy7@users.noreply.github.com>
2025-06-27 11:12:32 -0300

    oiii

--- src/pages/acesso/login/PaginaPrincipal.jsx
+++ src/pages/acesso/login/PaginaPrincipal.jsx
@@ -3,29 +3,57 @@ import { useState } from "react"
 export default function PaginaPrincipal(){
     const[PaginaCriarTarefa, setPaginaCriarTarefa] = useState(false)
     const[CategoriaTarefa, setCategoriaTarefa] = useState(false)
+    const[TipoTarefa, setTipoTarefa] = useState(false)
+    const[PaginaCriarCategoria, setPaginaCriarCategoria] = useState(false)
+    const [NomeCategoria, setNomeCategoria] = useState("");
+
     function CriarTarefa(){
         setPaginaCriarTarefa(true)
     }
-    function EscolherCategoria(){
+    function EscolherCategoriaTarefa(){
         setCategoriaTarefa(true)
     }
+    function EscolherTipoTarefa(){
+        setTipoTarefa(true)
+    }
+    function CriarCategoria(){
+        setPaginaCriarCategoria(true)
+    }
+    let Tipo = null
     let Categoria = null
-    let NomedaTarefa = null
+    let TarefaCriar = null
+    let CategoriaCriar = null
     if (PaginaCriarTarefa){
-        NomedaTarefa = 
+        TarefaCriar = 
         <>
         <input type="text" id="NomeTarefa"/>
-        <button onClick={EscolherCategoria}>EscolherCategoria</button>
+        <button onClick={EscolherCategoriaTarefa}>EscolherCategoria</button>
+        <button onClick={EscolherTipoTarefa}>EscolherTipo</button>
         </>
 
 
         if (CategoriaTarefa){
             Categoria = 
-            <>            
+            <>
+                <button onClick={CriarCategoria}>Novo</button>
+                <button onClick={NomeCategoria}/>
+            </>
+
+
+        }
+        if (TipoTarefa){
+            Tipo = 
+            <>
             <button>Fixo</button>
             <button>Variavel</button>
             </>
-
+        }
+        if (PaginaCriarCategoria){
+            CategoriaCriar = 
+            <>
+            <input type="text" value={NomeCategoria} onChange={(e) => setNomeCategoria(e.target.value)}/>
+            <button>adicionar</button>
+            </>
         }
     }
     function EditarTarefa(){
@@ -42,8 +70,10 @@ export default function PaginaPrincipal(){
         <input type="text" name="BarraPesquisa" id="Pesquisar" placeholder="pesquisar"/>
         <button onClick={CriarTarefa}>+</button>
         <button onClick={Filtro}>Filtrar</button>
-        {NomedaTarefa}
+        {TarefaCriar}
         {Categoria}
+        {Tipo}
+        {CategoriaCriar}
         </>
     )
 
